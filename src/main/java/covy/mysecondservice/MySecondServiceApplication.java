765bc05e9989a48a39d62284243a9c983dd937ea
박준혁/PD프로젝트 <junni802@gsretail.com>
2025-02-12 14:42:20 +0900

    feat: add msa-second-service project

---
+++ src/main/java/covy/mysecondservice/MySecondServiceApplication.java
@@ -0,0 +1,13 @@
+package covy.mysecondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecondServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySecondServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/covy/mysecondservice/MySecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.mysecondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
