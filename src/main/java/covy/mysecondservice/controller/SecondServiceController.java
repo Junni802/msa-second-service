444b2c27fc3fda4b9a26c4aca5f1b141e215baa4
박준혁/PD프로젝트 <junni802@gsretail.com>
2025-02-13 15:31:10 +0900

    Feat: add

--- src/main/java/covy/mysecondservice/controller/SecondServiceController.java
+++ src/main/java/covy/mysecondservice/controller/SecondServiceController.java
@@ -1,6 +1,7 @@
 package covy.mysecondservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in Second Service.";
     }
 
 }
